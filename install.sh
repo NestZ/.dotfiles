77c992bf80d60a88c89ba1cee56abf29144c4586
NestZ <NestZ671@gmail.com>
2020-06-10 12:03:37 +0700

    add tpm

--- install.sh
+++ install.sh
@@ -7,9 +7,6 @@ sudo apt-get upgrade -y
 sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
 	       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 
-# tmux themepack
-git clone https://github.com/jimeh/tmux-themepack.git ~/.tmux-themepack
-
 # git config
 git config --global user.email "NestZ671@gmail.com"
 git config --global user.name "NestZ"
@@ -25,6 +22,12 @@ cp init.vim ../.config/nvim/
 cp .tmux.conf ../
 cp .zshrc ../
 cp backup_conf.sh ../
+
+# tmux themepack
+git clone https://github.com/jimeh/tmux-themepack.git ~/.tmux-themepack
+
+# tmux package manager
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 
 # reload tmux environment
 tmux source ~/.tmux.conf
