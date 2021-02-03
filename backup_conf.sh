f51d34225fc0475df5d425467f09e93d4b33e87c
NestZ <NestZ671@gmail.com>
2021-02-03 12:36:40 +0700

    change dir name

--- backup_conf.sh
+++ backup_conf.sh
@@ -1,5 +1,5 @@
-cp .config/nvim/init.vim ./dotfiles/
-cp .zshrc ./dotfiles/
-cp .tmux.conf ./dotfiles/
-cp .config/nvim/coc-settings.json ./dotfiles/
-cp backup_conf.sh ./dotfiles/
+cp .config/nvim/init.vim ./.dotfiles/
+cp .zshrc ./.dotfiles/
+cp .tmux.conf ./.dotfiles/
+cp .config/nvim/coc-settings.json ./.dotfiles/
+cp backup_conf.sh ./.dotfiles/
